7f1e548979f4054d27a5f1360befc4196b37975b
JohnVitor-Dev <johnvitoralves39@gmail.com>
2023-08-24 07:38:53 -0300

    Adicionar arquivos de projeto.

---
+++ LearningServerRebuild/Data/ProjectContext.cs
@@ -0,0 +1,27 @@
+﻿using LearningServerRebuild.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Data
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> options)
+            : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddEnvironmentVariables()
+            .AddJsonFile("appsettings.json", false, true)
+            .Build();
+
+            string connectionString = Environment.GetEnvironmentVariable("CONNECTIONSTRINGS_PROJECTCONTEXT");
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+
+        public DbSet<Users> User { get; set; }
+        public DbSet<Products> Product { get; set; }
+
+    }
+}
